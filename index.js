11d23de0d0f8ce770600f1d45a0a76211ae1d560
joao nicz <jp3brnz@gmail.com>
2022-03-10 17:13:50 +1300

    Session 2
    Organized tables added classes

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
-const books = [
+const booksAction = [
     {
-        name: 'book 1',
-        author: 'some author 1',
+        name: 'Action Book 1',
+        author: 'Some Author 1',
     },
     {
-        name: 'book 2',
-        author: 'some author 2',
+        name: 'Action Book 2',
+        author: 'Some Author 2',
     },
 ];
 
-let htmlBody = '';
-for ( let i = 0; i < books.length; i++ ) {
-    for ( let j = 0; j <books[i].values(); j++) {
-        htmlBody += '<tr><td>' + books[i].name +  '</td></tr>';
+const booksAdventure = [
+    {
+        name: 'Adenture Book 1',
+        author: 'Some Author 1',
+    },
+    {
+        name: 'Adventure Book 2',
+        author: 'Some Author 2',
+    },
+];
+
+function renderTable(bookParam) {
+    if (bookParam.length === 0) {
+        console.error('Books has an unexpected size of 0');
+        return ;
     }
+    ;
+
+    //render the html table body tag
+    let htmlBody = '';
+    //the for loops serve as an activator for the table
+    for (let i = 0; i < bookParam.length; i++) {
+        const book = bookParam[i];
+        const bookArray = Object.values(book);
+
+        htmlBody += '<tr>'
+        for (let j = 0; j < bookArray.length; j++) {
+            htmlBody += `<td>${bookArray[j]}</td>`;
+        }
+        htmlBody += '</tr>';
+    }
+
+    //render the html table head tag
+    let htmlHead = '';
+
+    const book = bookParam[0] ?? {};
+    const bookArray = Object.keys(book);
+
+    //this creates a row first and then creates the headings inside of the row.
+    htmlHead += '<tr>'
+    for (let j = 0; j < bookArray.length; j++) {
+        htmlHead += `<th>${bookArray[j]}</th>`;
+    }
+    htmlHead += '</tr>'
+
+    return `<table class="table-book"><thead class="table-book-head">${htmlHead}   </thead><tbody>${htmlBody}   </tbody></table>`;
+
 
 }
 
-document.body.innerHTML = '<table>' + htmlBody + '</table>';
+document.body.innerHTML = `<div class="d-inline mr-2">${renderTable(booksAction)}</div> <div class="d-inline">${renderTable(booksAdventure)}</div>`;
+
+
+
+

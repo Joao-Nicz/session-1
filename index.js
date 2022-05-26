ac1e756f3fc158e4b22fc362538e09872f578a03
joao nicz <jp3brnz@gmail.com>
2022-05-26 16:04:29 +1200

    opening modules

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+//--------------------------
+// Project Variables
+//--------------------------
+const bookShiftQuantity = 3;
+
+
+//--------------------------
+// Functionality
+//--------------------------
 const renderLabelValue = (label, value, maxLength) => `
     <div>
         <span class="label">${label}</span>
         <span class="label-text">${
-    maxLength && value.length - 1 > maxLength ?
-        value.substring(0, maxLength - 3) + '...'
-        : value
+  maxLength && value.length - 1 > maxLength ?
+    value.substring(0, maxLength - 3) + '...'
+    : value
 }
         </span>
     </div>
@@ -13,12 +22,17 @@ const renderLabelValue = (label, value, maxLength) => `
 const renderBookFigure = (bookParam) => `
     <figure class="fig-book">
         <div class="fig-img">
-            <img src="./img/books/${bookParam.image ?? 'default-book.png'}" alt="Book Image"/>
+            <img src="./img/books/${bookParam.id +'.png'?? 'missing.png'}" alt="Book Image"/>
         </div>
         <figcaption>
             ${renderLabelValue('Name', bookParam.name ?? 'Unknown Book')}
             ${renderLabelValue('Author', bookParam.author ?? 'Unknown Author')}
-            ${bookParam.description ? renderLabelValue('Description', bookParam.description, 50) : ''}
+            <span class="description-partial">
+                ${bookParam.description ? renderLabelValue('Description', bookParam.description, 50) : ''}
+            </span>
+            <span class="description-full">
+                ${renderLabelValue('Description', bookParam.description)}
+            </span>
         </figcaption>
     </figure>
 `;
@@ -26,16 +40,16 @@ const renderBookFigure = (bookParam) => `
 // action, data...
 const renderBookCategory = (bookType, data) => `
     <div class="book-category" data-category="${bookType}">
-        <button onclick="onButtonClick('${bookType}', 1)" class="previous">
+        <button onclick="onButtonPreviousClick('${bookType}')" class="previous">
             &lt;
         </button>
-        <button onclick="onButtonClick('${bookType}', -1)" class="next">
+        <button onclick="onButtonNextClick('${bookType}')" class="next">
             &gt;
         </button>
         <h2>${bookType}</h2>
         <div class="flex-grid" id="row">
             ${data.map(book => `
-                <div class="mr-2">
+                <div class="mr-2" data-id="${book.id}" onclick="onBookClick('${book.id}')">
                     ${renderBookFigure(book)}
                 </div>
             `).join('')}
@@ -43,44 +57,88 @@ const renderBookCategory = (bookType, data) => `
     </div>
 `;
 
-// int
-// --> 1, 2, 3, 100, 200, -2, 0, -100
-// float
-// --> 1, 2, -100, 0, 1.2, 3.5, 2.5
-
-const onButtonClick = (bookType, directionMultiplier) => {
-    const element = document.querySelector(`.book-category[data-category="${bookType}"] div`);
-    const bookWidth = document.querySelector('.fig-book').offsetWidth;
-    const currentOffset = parseFloat(element.style.marginLeft !== '' ? element.style.marginLeft : 0);
-    const row = element;
-    const allChildren = row.getElementsByClassName('mr-2').length;
-    const newOffset = `${currentOffset + directionMultiplier * bookWidth *3 }`;
-    const scrollLimit = `${allChildren * 161}`
-
-
-    if (newOffset > 0) {
-
-    }else if (newOffset *-1 > scrollLimit){
-    }else{
-        element.style.marginLeft = `${newOffset}px`;
-    };
-
-
-
-
-    // -> Only allow positive
-    // -> Total width of element - offset = what is on screen > window width
-    // document.querySelector(`.book-category[data-category="action"] div`).offsetWidth
-    // window.innerWidth
-    console.log(`${bookType}, offset = ${newOffset}, window width = ${window.innerWidth}, scroll limit = ${scrollLimit}`);
-    console.log(currentOffset, allChildren)
+/*
+How you made that interaction.
+ */
+const onButtonPreviousClick = (bookType) => {
+  shiftBooksRight(bookType, -1);
+};
+const onButtonNextClick = (bookType) => {
+  shiftBooksRight(bookType, 1);
+};
+const onBookClick = (bookId, bookType) => {
+  bookFocus(bookId, bookType);
 };
 
+const bookFocus = (bookId, bookType) => {
+  const book = document.querySelector(`*[data-id="${bookId}"] .fig-book`);
+  book.classList.toggle('active');
+
+  // Some active state DONE
+  // - Book to right DONE
+  // - Text to left DONE 
+  // Description show everything DONE
+  // Description scroll
+  // Click again, goes back to original
+  // Click another, goes back to original
+}
+
+/*
+Cares about moving the books to the left or right.
+ */
+const shiftBooksRight = (bookType, directionMultiplier) => {
+  // The size of the category box on the screen.
+  const elementSection = document.querySelector(`.book-category[data-category="${bookType}"]`);
+
+  // The size of the row, and everything hiding off the edge.
+  const elementRow = document.querySelector(`.book-category[data-category="${bookType}"] div`);
+
+  // Multiply by -1, so that +1 = move to right, and -1 = move to left.
+  const currentOffset = -1 * parseFloat(elementRow.style.marginLeft !== '' ? elementRow.style.marginLeft : 0);
+
+  const bookWidth = document.querySelector('.fig-book').clientWidth;
+  const viewWidth = elementSection.clientWidth;
+  const rowWidth = elementRow.scrollWidth;
+
+  const newOffset = getBookShiftOffset(rowWidth, viewWidth, currentOffset, directionMultiplier * bookShiftQuantity * bookWidth);
+  elementRow.style.marginLeft = `${-1 * newOffset}px`;
+
+  const maxOffset = getBookShiftOffset(rowWidth, viewWidth, 0, 9999);
+  const minOffset = getBookShiftOffset(rowWidth, viewWidth, 0, -9999);
+
+  const buttonRight = elementSection.querySelector('.next');
+  const buttonLeft = elementSection.querySelector('.previous');
+
+  buttonRight.style.display = parseFloat(newOffset) === parseFloat(maxOffset) ? 'none' : 'block';
+  buttonLeft.style.display = parseFloat(newOffset) === parseFloat(minOffset) ? 'none' : 'block';
+}
+
+/*
+Cares about doing the calculation and what the new offset is.
+ */
+const getBookShiftOffset = (rowWidth, viewWidth, currentOffset, offsetChange) => {
+  const minOffset = 0;
+  const maxOffset = rowWidth - viewWidth > 0 ? rowWidth - viewWidth : 0;
+  const newOffset = `${currentOffset + offsetChange}`;
+
+  console.log('Calculating book shift offset:', {rowWidth, viewWidth, currentOffset, maxOffset, minOffset});
+
+  // If the scroll goes too far to the left, just make it the minimum offset.
+  if (newOffset < minOffset) {
+    return minOffset;
+  }
+  // If the scroll goes too far to the right, just make it the maximum offset.
+  else if (newOffset > maxOffset) {
+    return maxOffset;
+  }
+  // Otherwise, just return the offset calculated.
+  return newOffset;
+}
+
 const bookTypes = [
-    'action',
-    'adventure',
-    'fantasy',
-    'drama'
+  'action',
+  'adventure',
+  'fantasy',
 ];
 
 // Functions can access global
@@ -88,18 +146,19 @@ const bookTypes = [
 
 
 bookTypes.map((bookType) =>
-    fetch(`./api/v1/books-${bookType}.json`)
-        .then(data => data.json())
-        .then(data => {
-            setTimeout(() => {
-                document.body.innerHTML += renderBookCategory(bookType, data);
-            }, 100);
-        })
-        .catch(error => {
-            console.error(error);
-            document.body.innerHTML += `An error occurred with the book type ${bookType}<br/>`;
-        })
-        .finally(() => {
-            document.body.innerHTML = document.body.innerHTML.replace('Loading...', '');
-        })
+  fetch(`./api/v1/books-${bookType}.json`)
+    .then(data => data.json())
+    .then(data => {
+      setTimeout(() => {
+        document.body.innerHTML += renderBookCategory(bookType, data);
+        shiftBooksRight(bookType, 0);
+      }, 100);
+    })
+    .catch(error => {
+      console.error(error);
+      document.body.innerHTML += `An error occurred with the book type ${bookType}<br/>`;
+    })
+    .finally(() => {
+      document.body.innerHTML = document.body.innerHTML.replace('Loading...', '');
+    })
 );
